5a432b21bf3a2c6d6932e12f55f40f40b531575f
fismael21 <fernandoismaelvirgo@gmail.com>
2022-10-15 13:21:20 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ allLinks.forEach(function (link) {
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    // Close mobile naviagtion
+    // Close mobile navigation
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
